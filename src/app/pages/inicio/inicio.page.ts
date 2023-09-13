4e67e2c660d2fcc4a483de1b6145a1a4bfacb36d
CesarHB <chenriquezbachmann@gmail.com>
2023-09-13 01:43:16 -0300

    agregado logos, y otros

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,15 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AnimationController} from '@ionic/angular';
+import { NivelEducacional } from 'src/app/model/nivel-educacional';
+import { Usuario } from 'src/app/model/usuario';
+import { ToastController } from '@ionic/angular'; // Permite mostrar mensajes emergente
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
 
-  constructor() { }
+export class InicioPage implements OnInit, AfterViewInit {
 
-  ngOnInit() {
+  @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
+  @ViewChild('itemNombre', { read: ElementRef }) itemNombre!: ElementRef;
+  @ViewChild('itemApellido', { read: ElementRef }) itemApellido!: ElementRef;
+  @ViewChild('itemEducacion', { read: ElementRef }) itemEducacion!: ElementRef;
+  @ViewChild('itemFechaNacimiento', { read: ElementRef }) itemFechaNacimiento!: ElementRef;
+
+  public usuario: Usuario;
+  public idNivelEducacional: number;
+
+  public nivelesEducacionales: NivelEducacional[] = NivelEducacional.getNivelesEducacionales();
+
+  constructor(
+    private activeroute: ActivatedRoute // Permite obtener los parámetros de la página login
+  , private router: Router // Permite navegar entre páginas
+  , private alertController: AlertController // Permite mostrar mensajes emergentes más complejos que Toast
+  , private animationController: AnimationController
+  , private toastController: ToastController)  { 
+
+    this.usuario = new Usuario('', '', '', '', '', '', 0, null);
+    this.idNivelEducacional = 0;
+
+    // Se llama a la ruta activa y se obtienen sus parámetros mediante una subscripcion
+    this.activeroute.queryParams.subscribe(params => { 
+
+      const nav = this.router.getCurrentNavigation();
+      if (nav) {
+        // Si tiene datos extra, se rescatan y se asignan a una propiedad
+        if (nav.extras.state) {
+          this.usuario = nav.extras.state['usuario'];
+          if (this.usuario.nivelEducacional !== undefined) {
+            this.idNivelEducacional = this.usuario.nivelEducacional.id;
+          }
+          return;
+        }
+      }
+      // Si no vienen datos extra desde la página anterior, quiere decir que el usuario
+      // intentó entrar directamente a la página home sin pasar por el login,
+      // de modo que el sistema debe enviarlo al login para que inicie sesión.
+      this.router.navigate(['/ingreso']);
+
+    });
   }
 
+  public ngOnInit(): void {
+  }
+
+  public ngAfterViewInit(): void {
+    if (this.itemTitulo) {
+      const animation = this.animationController
+        .create()
+        .addElement(this.itemTitulo.nativeElement)
+        .iterations(Infinity)
+        .duration(6000)
+        .fromTo('transform', 'translate(0%)', 'translate(100%)')
+        .fromTo('opacity', 0.2, 1);
+
+      animation.play();
+    }
+  }
+
+  async mostrarMensaje(mensaje: string, duracion?: number) {
+    // Permite mostrar un mensaje emergente que dura unos pocos segundos y desaparece. El mensaje es asincrónico, 
+    // los que permite que el mensaje se pueda ver incluso cuando ya ha cambiado a la siguiente página.
+    const toast = await this.toastController.create({
+        message: mensaje,
+        duration: duracion? duracion: 2000
+      });
+    toast.present();
+  }
+
+  public cerrarSesion(): void {
+    this.router.navigate(['/ingreso']);
+    this.mostrarMensaje("¡Hasta luego " + this.usuario.nombre + ' ' + this.usuario.apellido + "!");
+  }
+
+  
 }
