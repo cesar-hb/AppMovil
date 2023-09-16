379b96bd4cc97015d71af183e35e1985902bf434
iahumadaz <i.ahumada@duocuc.cl>
2023-09-15 23:15:47 -0300

    pregunta secreta

--- src/app/pages/pregunta/pregunta.page.ts
+++ src/app/pages/pregunta/pregunta.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'; // Permite navegar y pasar parámetros extra entre páginas
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-pregunta',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pregunta.page.scss'],
 })
 export class PreguntaPage implements OnInit {
+  
+  public usuario: Usuario;
+  public respuesta: string= '';
+  
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private router: Router) {
 
-  constructor() { }
+    this.activatedRoute.queryParams.subscribe(params => { 
 
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.usuario = this.router.getCurrentNavigation().extras.state.usuario;
+    } else{
+          this.router.navigate(['/login']);
+    }
+  });
+}
   ngOnInit() {
   }
-
+  public
+  public aPaginaComprovacion():void{
+    if(this.usuario.respuestaSecreta === this.respuesta){
+      alert('Respuesta Correecta. Tu clave es '+this.usuario.password);
+    }
+    else{
+      alert('Incorrecto')
+      };
+      this.router.navigate(['/pregunta'], navigationExtras);
+    }
+  }
 }
