3be76d5980605cd17252bd4caa7a058f0080e8e9
CesarHB <chenriquezbachmann@gmail.com>
2023-09-18 16:55:51 -0300

    width a logos de pregunta y correcto

--- src/app/pages/pregunta/pregunta.page.ts
+++ src/app/pages/pregunta/pregunta.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router'; // Permite navegar y pasar parámetros extra entre páginas
-import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-pregunta',
@@ -8,34 +6,10 @@ import { Usuario } from 'src/app/model/usuario';
   styleUrls: ['./pregunta.page.scss'],
 })
 export class PreguntaPage implements OnInit {
-  
-  public usuario: Usuario;
-  public respuesta: string= '';
-  
-  constructor(
-    private activatedRoute: ActivatedRoute, 
-    private router: Router) {
 
-    this.activatedRoute.queryParams.subscribe(params => { 
+  constructor() { }
 
-      if (this.router.getCurrentNavigation().extras.state) {
-        this.usuario = this.router.getCurrentNavigation().extras.state.usuario;
-    } else{
-          this.router.navigate(['/login']);
-    }
-  });
-}
   ngOnInit() {
   }
-  public
-  public aPaginaComprovacion():void{
-    if(this.usuario.respuestaSecreta === this.respuesta){
-      alert('Respuesta Correecta. Tu clave es '+this.usuario.password);
-    }
-    else{
-      alert('Incorrecto')
-      };
-      this.router.navigate(['/pregunta'], navigationExtras);
-    }
-  }
+
 }
